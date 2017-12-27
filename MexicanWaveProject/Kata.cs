54b4ca97277531e9095f073568a037b63e6cf0de
k7173968 <k7173968@gmail.com>
2017-12-27 17:49:07 +0800

    Check

---
+++ MexicanWaveProject/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MexicanWaveProject
+{
+    public class Kata
+    {
+        public List<string> Wave(string str)
+        {
+
+            var waving = new List<string>();
+            if (string.IsNullOrEmpty(str))
+            {
+                return waving;
+            }
+            return LetStrWave(str, waving);
+        }
+
+        private static List<string> LetStrWave(string str, List<string> waving)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                waving.Add(str.Remove(i, 1).Insert(i, str.Substring(i, 1).ToUpper()));
+            }
+            waving.RemoveAll(x => x.Equals(str));
+            return waving;
+        }
+    }
+}
--- MexicanWaveProject/UnitTest1.cs
+++ MexicanWaveProject/UnitTest1.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using MexicanWaveProject;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using CollectionAssert = Microsoft.VisualStudio.TestTools.UnitTesting.CollectionAssert;
 
-namespace MexicanWaveProject
+namespace MexicanWave
 {
     [TestClass]
     public class UnitTest1
@@ -9,6 +14,48 @@ namespace MexicanWaveProject
         [TestMethod]
         public void TestMethod1()
         {
+            Kata kata = new Kata();
+            var result = new List<string> { };
+            CollectionAssert.AreEqual(result, kata.Wave(""), "it should return '" + result + "'");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Kata kata = new Kata();
+            List<string> result = new List<string> { "H" };
+            CollectionAssert.AreEqual(result, kata.Wave("h"), "it should return '" + result + "'");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Kata kata = new Kata();
+            List<string> result = new List<string> { "He", "hE" };
+            CollectionAssert.AreEqual(result, kata.Wave("he"), "it should return '" + result + "'");
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Kata kata = new Kata();
+            List<string> result = new List<string> { "H e", "h E" };
+            CollectionAssert.AreEqual(result, kata.Wave("h e"), "it should return '" + result + "'");
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Kata kata = new Kata();
+            List<string> result = new List<string> { "$He", "$hE" };
+            CollectionAssert.AreEqual(result, kata.Wave("$he"), "it should return '" + result + "'");
+        }
+        [TestMethod]
+        public void BasicTest1()
+        {
+            Kata kata = new Kata();
+            List<string> result = new List<string> { "Hello", "hEllo", "heLlo", "helLo", "hellO" };
+            CollectionAssert.AreEqual(result, kata.Wave("hello"), "it should return '" + result + "'");
         }
     }
 }
